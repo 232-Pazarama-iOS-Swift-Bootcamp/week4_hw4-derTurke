594ee97a61472695ec3b634df178bcd5b7982a65
GÜRHAN YUVARLAK <76452852+derTurke@users.noreply.github.com>
2022-10-17 22:08:37 +0300

    Firebase, third party libraries and initial view added.

--- FlickrApi/FlickrApi/App/Delegates/AppDelegate.swift
+++ FlickrApi/FlickrApi/App/Delegates/AppDelegate.swift
@@ -6,15 +6,35 @@
 //
 
 import UIKit
-
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        setupFirebase()
+        setupWindow()
+        
         return true
+    }
+    
+    // Firebase setup
+    private func setupFirebase() {
+        FirebaseApp.configure()
+        _ = Firestore.firestore()
+    }
+    
+    // First page setup
+    private func setupWindow() {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = WelcomeViewController()
+        window.rootViewController = UINavigationController(rootViewController: viewController)
+        window.makeKeyAndVisible()
+        self.window = window
     }
 }
 
---
+++ FlickrApi/FlickrApi/Scenes/Welcome/Controller/WelcomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  WelcomeViewController.swift
+//  FlickrApi
+//
+//  Created by GÜRHAN YUVARLAK on 17.10.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    //MARK: - Properties
+    
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK: - Methods
+
+}
