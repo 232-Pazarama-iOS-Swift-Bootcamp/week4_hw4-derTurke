dd7e5fc18ec1f7ef2700a689c920fe656158ac13
GÜRHAN YUVARLAK <76452852+derTurke@users.noreply.github.com>
2022-10-17 20:03:22 +0300

    Project setup.

---
+++ FlickrApi/FlickrApi/App/Delegates/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  FlickrApi
+//
+//  Created by GÜRHAN YUVARLAK on 17.10.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
